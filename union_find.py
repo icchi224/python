132409361efbb07b4ee41d5656f588ea5e74b58e
ichimura <sd20015@tti-j.net>
2022-10-27 19:53:35 +0900

    aaa

--- union_find.py
+++ union_find.py
@@ -1,24 +1,51 @@
-def ll(): return list(map(int, input().split()))
-import sys
-sys.setrecursionlimit(10000000)
+from collections import defaultdict
 
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
 
-n,q=ll()
-par=[i for i in range(n)]
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
 
-#Union find 2点間が連結かどうかをO(logn)で判定
-def root(x):
-    if par[x]==x:
-        return x
-    else:
-        par[x]=root(par[x])
-        return par[x]
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
 
-for _ in range(q):
-    x,y=ll()
-    rx=root(x)
-    ry=root(y)
-    if rx!=ry:
-        par[rx]=ry  #根をryに変更
+        if x == y:
+            return
 
-print(par)
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
